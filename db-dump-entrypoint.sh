f3eef108d1b260d4458528f870e5b58a2b4e4d4c
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2025-10-01 18:45:01 +0200

    Use logrotate to compress and repeat dumps

--- db-dump-entrypoint.sh
+++ db-dump-entrypoint.sh
@@ -6,24 +6,48 @@
 set -x
 set -e
 
-# Install and configure cron
+# Install cron, logrotate and tini
 apt-get update
-apt-get install -y cron tini
-if [ ! -f /etc/cron.daily/dump ]; then
-    cat > /etc/cron.daily/dump <<EOF
+apt-get install -y cron logrotate  tini
+
+# Configure db dump script, save container env vars in script to simplify
+# container configuration
+if [ ! -f /db_dump ]; then
+    cat > /db_dump <<EOF
 #!/usr/bin/env sh
 $(export)
-pg_dump -f /var/lib/postgresql/data/backup.sql
+pg_dump -f /var/lib/postgresql/data/db_dump.sql
 EOF
-	chmod 755 /etc/cron.daily/dump
+	chmod 755 /db_dump
+fi
+
+# Use logrotate to keep multiple backups, compress them, and don't
+# leak too much old data
+if [ ! -f /etc/logrotate.d/postgres ]; then
+    cat > /etc/logrotate.d/postgres <<EOF
+/var/lib/postgresql/data/*.sql {
+	daily
+	compress
+	rotate 10
+	missingok
+	create 0644 postgres postgres
+    postrotate
+    su -c /db_dump - postgres
+    endscript
+}
+EOF
 fi
 
 # Make sure that no other cron job needs to be executed
 rm /etc/cron.hourly/* -f
-find /etc/cron.daily/ ! -name 'dump' -type f -exec rm -f {} +
+find /etc/cron.daily/ ! -name 'logrotate' -type f -exec rm -f {} +
 rm /etc/cron.weekly/* -f
 rm /etc/cron.monthly/* -f
 rm /etc/cron.yearly/* -f
+
+# Validate that the db_dump script works and generate a first dump
+chown postgres -R /var/lib/postgresql/data
+su -c /db_dump - postgres
 
 # Run cron as a main process
 exec /usr/bin/tini -- cron -f
